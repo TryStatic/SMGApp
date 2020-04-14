29ff2a44d6c9edab56ac4d5589b0f5b7cee9d1db
Static <jimakosfb@gmail.com>
2020-04-14 20:16:09 +0300

    Added data service interface

---
+++ SMGApp.Domain/Services/IDataService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SMGApp.Domain.Services
+{
+    internal interface IDataService<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Create(T entity);
+        Task<T> Update(int id, T entity);
+        bool Delete(int id);
+    }
+}
