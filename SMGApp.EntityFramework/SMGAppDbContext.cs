a6bd1e8abebbc8c1f3dd80004338678168482de5
Static <jimakosfb@gmail.com>
2020-04-14 20:29:27 +0300

    config for ef core to use sqllite

--- SMGApp.EntityFramework/SMGAppDbContext.cs
+++ SMGApp.EntityFramework/SMGAppDbContext.cs
@@ -8,5 +8,11 @@ namespace SMGApp.EntityFramework
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<ServiceItem> ServiceItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=SMGData.dat;Version=3;");
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
