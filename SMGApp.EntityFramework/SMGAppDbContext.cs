256cf02b560a6cc87acc22e695d3ce7a72125ec6
Static <jimakosfb@gmail.com>
2020-04-14 20:22:27 +0300

    Added AppDbContext class and DbSets for the models

---
+++ SMGApp.EntityFramework/SMGAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMGApp.Domain.Models;
+
+namespace SMGApp.EntityFramework
+{
+    public class SMGAppDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ServiceItem> ServiceItems { get; set; }
+    }
+}
