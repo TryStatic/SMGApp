17ffeae25edf0fcce6ede58b40b8751334eb9a1e
Static <jimakosfb@gmail.com>
2020-04-16 03:58:51 +0300

    Made Customers View to be the Startup View

--- SMGApp.WPF/ViewModels/MainViewModel.cs
+++ SMGApp.WPF/ViewModels/MainViewModel.cs
@@ -5,5 +5,11 @@ namespace SMGApp.WPF.ViewModels
     public class MainViewModel : ViewModelBase
     {
         public INavigator Navigator { get; set; } = new Navigator();
+
+        public MainViewModel()
+        {
+            // Startup View
+            Navigator.UpdateCurrentViewModelCommand.Execute(ViewType.Customer);
+        }
     }
 }
