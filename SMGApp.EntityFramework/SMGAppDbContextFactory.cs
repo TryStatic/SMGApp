3a60b6ad4491095cc4800b62d448d5a21cc020f5
Static <jimakosfb@gmail.com>
2020-04-24 19:36:45 +0300

    Explicit Path for the Data Source

--- SMGApp.EntityFramework/SMGAppDbContextFactory.cs
+++ SMGApp.EntityFramework/SMGAppDbContextFactory.cs
@@ -9,10 +9,14 @@ namespace SMGApp.EntityFramework
 {
     public class SMGAppDbContextFactory : IDesignTimeDbContextFactory<SMGAppDbContext>
     {
+        private readonly string _path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Substring(6);
+
+
         public SMGAppDbContext CreateDbContext(string[] args = null)
         {
             DbContextOptionsBuilder<SMGAppDbContext> options = new DbContextOptionsBuilder<SMGAppDbContext>();
-            options.UseSqlite("Data Source=SMGData.dat;");
+            options.UseSqlite($"Data Source={_path}\\SMGData.dat;");
+
             return new SMGAppDbContext(options.Options);
         }
 
