b3a953739b47ea0bbc084ea8494addfa8aa123ee
Static <jimakosfb@gmail.com>
2020-04-24 19:22:55 +0300

    Updated ServiceItemModel

--- SMGApp.Domain/Models/ServiceItem.cs
+++ SMGApp.Domain/Models/ServiceItem.cs
@@ -5,18 +5,29 @@ namespace SMGApp.Domain.Models
     public class ServiceItem : DomainObject
     {
         public Customer Customer { get; set; }
+        public string DeviceDescription { get; set; }
         public string DamageDescription { get; set; }
+        public string DevicePassword { get; set; }
+        public string SimPassword { get; set; }
+        public string DeviceAccountUsername { get; set; }
+        public string DeviceAccountPassword { get; set; }
+        public bool ChargerIncluded { get; set; }
+        public bool BagIncluded { get; set; }
+        public bool CaseIncluded { get; set; }
         public DateTime DateAdded { get; set; } = DateTime.Now;
         public DateTime DateUpdated { get; set; }
+        
         public ServiceState State { get; set; }
+        public string Notes { get; set; }
 
         public string CustomerDetails => Customer != null ? $"{Customer.LastName} {Customer.FirstName}" : "";
     }
 
     public enum ServiceState
     {
-        Stored,
+        Arrived,
         Fixed,
-        Delivered
+        Delivered,
+        Issue
     }
 }
