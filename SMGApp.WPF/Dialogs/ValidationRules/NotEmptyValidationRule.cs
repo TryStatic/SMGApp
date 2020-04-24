477880f645680eee7c579e7599badd5b241abe35
Static <jimakosfb@gmail.com>
2020-04-24 09:29:46 +0300

    Moved Validation Rules to it's own namespace

---
+++ SMGApp.WPF/Dialogs/ValidationRules/NotEmptyValidationRule.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace SMGApp.WPF.Dialogs.ValidationRules
+{
+    public class NotEmptyValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(value == null) return new ValidationResult(false, "Field is required.");
+            if (!(value is string str)) return new ValidationResult(false, "Field is required.");
+            if (string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str)) return new ValidationResult(false, "Field is required.");
+            return ValidationResult.ValidResult;
+        }
+    }
+}
