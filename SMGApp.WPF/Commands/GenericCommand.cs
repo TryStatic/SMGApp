6a239d3e0461e31a4f3c0787aed7f889cf8987c6
Static <jimakosfb@gmail.com>
2020-05-02 15:08:24 +0300

    Added GenericCommand to use for cmds

---
+++ SMGApp.WPF/Commands/GenericCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SMGApp.WPF.Commands
+{
+    public class GenericCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public GenericCommand(Action<object> execute) : this(execute, null)
+        {
+        }
+
+        public GenericCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? (x => true);
+        }
+
+        public bool CanExecute(object parameter) => _canExecute(parameter);
+
+        public void Execute(object parameter) => _execute(parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public void Refresh() => CommandManager.InvalidateRequerySuggested();
+    }
+}
