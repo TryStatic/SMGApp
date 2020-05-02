305a59fc3d6d32785dda20b7425238f2dd0d5668
Static <jimakosfb@gmail.com>
2020-05-02 13:05:12 +0300

    ServiceItemsDataService should be in its own file

--- SMGApp.EntityFramework/Services/GenericDataServices.cs
+++ SMGApp.EntityFramework/Services/GenericDataServices.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
@@ -8,49 +7,6 @@ using SMGApp.Domain.Services;
 
 namespace SMGApp.EntityFramework.Services
 {
-    public class ServiceItemsDataService : GenericDataServices<ServiceItem>
-    {
-        public ServiceItemsDataService(SMGAppDbContextFactory contextFactory) : base(contextFactory)
-        {
-        }
-
-        public override async Task<IEnumerable<ServiceItem>> GetAll()
-        {
-            await using SMGAppDbContext context = ContextFactory.CreateDbContext();
-            IEnumerable<ServiceItem> entities = await context.Set<ServiceItem>().Include(b => b.Customer).ToListAsync();
-            return entities;
-        }
-
-        public async Task<IEnumerable<ServiceItem>> GetByCustomer(int customerID)
-        {
-            await using SMGAppDbContext context = ContextFactory.CreateDbContext();
-            IEnumerable<ServiceItem> entities = await context.Set<ServiceItem>().Include(b => b.Customer).Where(c => c.Customer.ID == customerID).ToListAsync();
-            return entities;
-        }
-
-        public override async Task<ServiceItem> Get(int id)
-        {
-            await using SMGAppDbContext context = ContextFactory.CreateDbContext();
-            ServiceItem entity = await context.Set<ServiceItem>().Include(c => c.Customer).FirstOrDefaultAsync(e => e.ID == id);
-            return entity;
-        }
-
-        public override Task<ServiceItem> Create(ServiceItem entity)
-        {
-            return base.Create(entity);
-        }
-
-        public override Task<ServiceItem> Update(int id, ServiceItem entity)
-        {
-            return base.Update(id, entity);
-        }
-
-        public override Task<bool> Delete(int id)
-        {
-            return base.Delete(id);
-        }
-    }
-
     public class GenericDataServices<T> : IDataService<T> where T : DomainObject
     {
         protected readonly SMGAppDbContextFactory ContextFactory;
---
+++ SMGApp.EntityFramework/Services/ServiceItemsDataService.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SMGApp.Domain.Models;
+
+namespace SMGApp.EntityFramework.Services
+{
+    public class ServiceItemsDataService : GenericDataServices<ServiceItem>
+    {
+        public ServiceItemsDataService(SMGAppDbContextFactory contextFactory) : base(contextFactory)
+        {
+        }
+
+        public override async Task<IEnumerable<ServiceItem>> GetAll()
+        {
+            await using SMGAppDbContext context = ContextFactory.CreateDbContext();
+            IEnumerable<ServiceItem> entities = await context.Set<ServiceItem>().Include(b => b.Customer).ToListAsync();
+            return entities;
+        }
+
+        public async Task<IEnumerable<ServiceItem>> GetByCustomer(int customerID)
+        {
+            await using SMGAppDbContext context = ContextFactory.CreateDbContext();
+            IEnumerable<ServiceItem> entities = await context.Set<ServiceItem>().Include(b => b.Customer).Where(c => c.Customer.ID == customerID).ToListAsync();
+            return entities;
+        }
+
+        public override async Task<ServiceItem> Get(int id)
+        {
+            await using SMGAppDbContext context = ContextFactory.CreateDbContext();
+            ServiceItem entity = await context.Set<ServiceItem>().Include(c => c.Customer).FirstOrDefaultAsync(e => e.ID == id);
+            return entity;
+        }
+
+        public override Task<ServiceItem> Create(ServiceItem entity)
+        {
+            return base.Create(entity);
+        }
+
+        public override Task<ServiceItem> Update(int id, ServiceItem entity)
+        {
+            return base.Update(id, entity);
+        }
+
+        public override Task<bool> Delete(int id)
+        {
+            return base.Delete(id);
+        }
+    }
+}
