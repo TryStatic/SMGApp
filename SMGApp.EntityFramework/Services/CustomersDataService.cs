44b1d36be0aecd3ea288ad18e1e6624432febf27
Static <jimakosfb@gmail.com>
2020-05-02 13:05:51 +0300

    Added overrides for the CustomersDataService in case I need them

--- SMGApp.EntityFramework/Services/CustomersDataService.cs
+++ SMGApp.EntityFramework/Services/CustomersDataService.cs
@@ -1,4 +1,6 @@
-﻿using SMGApp.Domain.Models;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SMGApp.Domain.Models;
 
 namespace SMGApp.EntityFramework.Services
 {
@@ -6,6 +8,32 @@ namespace SMGApp.EntityFramework.Services
     {
         public CustomersDataService(SMGAppDbContextFactory contextFactory) : base(contextFactory)
         {
+
+        }
+
+        public override Task<IEnumerable<Customer>> GetAll()
+        {
+            return base.GetAll();
+        }
+
+        public override Task<Customer> Get(int id)
+        {
+            return base.Get(id);
+        }
+
+        public override Task<Customer> Create(Customer entity)
+        {
+            return base.Create(entity);
+        }
+
+        public override Task<Customer> Update(int id, Customer entity)
+        {
+            return base.Update(id, entity);
+        }
+
+        public override Task<bool> Delete(int id)
+        {
+            return base.Delete(id);
         }
     }
 }
