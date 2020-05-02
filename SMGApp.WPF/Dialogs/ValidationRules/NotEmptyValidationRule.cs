ec617b428dd610f2c7d0ce34c5384d729709c650
Static <jimakosfb@gmail.com>
2020-05-02 14:06:40 +0300

    Validation Rules translation and additions

--- SMGApp.WPF/Dialogs/ValidationRules/NotEmptyValidationRule.cs
+++ SMGApp.WPF/Dialogs/ValidationRules/NotEmptyValidationRule.cs
@@ -1,5 +1,4 @@
 ﻿using System.Globalization;
-using System.Linq;
 using System.Windows.Controls;
 
 namespace SMGApp.WPF.Dialogs.ValidationRules
@@ -8,9 +7,9 @@ namespace SMGApp.WPF.Dialogs.ValidationRules
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            if (value == null) return new ValidationResult(false, "Field is required.");
-            if (!(value is string str)) return new ValidationResult(false, "Field is required.");
-            if (string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str)) return new ValidationResult(false, "Field is required.");
+            if (value == null) return new ValidationResult(false, "ΤΟ ΠΕΔΙΟ ΔΕΝ ΜΠΟΡΕΙ ΝΑ ΕΙΝΑΙ ΚΕΝΟ");
+            if (!(value is string str)) return new ValidationResult(false, "ΤΟ ΠΕΔΙΟ ΔΕΝ ΜΠΟΡΕΙ ΝΑ ΕΙΝΑΙ ΚΕΝΟ");
+            if (string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str)) return new ValidationResult(false, "ΤΟ ΠΕΔΙΟ ΔΕΝ ΜΠΟΡΕΙ ΝΑ ΕΙΝΑΙ ΚΕΝΟ");
             return ValidationResult.ValidResult;
         }
     }
---
+++ SMGApp.WPF/Dialogs/ValidationRules/OnlyNumbersValidationRule.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace SMGApp.WPF.Dialogs.ValidationRules
+{
+    public class OnlyNumbersValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null) return ValidationResult.ValidResult;
+            if (!(value is string str)) return new ValidationResult(false, "ΤΟ ΠΕΔΙΟ ΠΡΕΠΕΙ ΝΑ ΠΕΡΙΕΧΕΙ ΜΟΝΟ ΑΡΙΘΜΟΥΣ");
+            if (string.IsNullOrEmpty(str)) return ValidationResult.ValidResult;
+            if(string.IsNullOrWhiteSpace(str)) return new ValidationResult(false, "ΤΟ ΠΕΔΙΟ ΠΡΕΠΕΙ ΝΑ ΠΕΡΙΕΧΕΙ ΜΟΝΟ ΑΡΙΘΜΟΥΣ");
+            if (!IsDigitsOnly(str)) return new ValidationResult(false, "ΤΟ ΠΕΔΙΟ ΠΡΕΠΕΙ ΝΑ ΠΕΡΙΕΧΕΙ ΜΟΝΟ ΑΡΙΘΜΟΥΣ");
+            return ValidationResult.ValidResult;
+        }
+
+        private static bool IsDigitsOnly(string str) => str != null && str.All(c => c >= '0' && c <= '9');
+    }
+
+
+    public class AtLeastTenCharactersValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null) return ValidationResult.ValidResult;
+            if (!(value is string str)) return new ValidationResult(false, "ΤΟ ΠΕΔΙΟ ΠΡΕΠΕΙ ΝΑ ΠΕΡΙΕΧΕΙ ΤΟΥΛΑΧΙΣΤΟΝ 10 ΧΑΡΑΚΤΗΡΕΣ");
+            if (string.IsNullOrEmpty(str)) return ValidationResult.ValidResult;
+            if(str.Length < 10) return new ValidationResult(false, "ΤΟ ΠΕΔΙΟ ΠΡΕΠΕΙ ΝΑ ΠΕΡΙΕΧΕΙ ΤΟΥΛΑΧΙΣΤΟΝ 10 ΧΑΡΑΚΤΗΡΕΣ");
+            return ValidationResult.ValidResult;
+        }
+
+    }
+
+
+}
