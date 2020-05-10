c52df05bde2468a95f80c5ac40251989de90c8ca
Static <jimakosfb@gmail.com>
2020-05-10 03:34:51 +0300

    Base for Create/Edit service Item

---
+++ SMGApp.WPF/Dialogs/ServiceDialogs/ServiceDialogView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SMGApp.WPF.Dialogs.ServiceDialogs
+{
+    /// <summary>
+    /// Interaction logic for SampleDialog.xaml
+    /// </summary>
+    public partial class ServiceDialogView : UserControl
+    {
+        public ServiceDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SMGApp.WPF/Dialogs/ServiceDialogs/ServiceViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SMGApp.WPF.Dialogs.ServiceDialogs
+{
+    public class ServiceViewModel : INotifyPropertyChanged
+    {
+        public string OperationName { get; set; }
+
+        private string _firstName;
+        public string FirstName
+        {
+            get => _firstName;
+            set => this.MutateVerbose(ref _firstName, value, RaisePropertyChanged());
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get => _lastName;
+            set => this.MutateVerbose(ref _lastName, value, RaisePropertyChanged());
+        }
+
+        private string _address;
+        public string Address
+        {
+            get => _address;
+            set => this.MutateVerbose(ref _address, value, RaisePropertyChanged());
+        }
+
+        private string _phoneNumber;
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set => this.MutateVerbose(ref _phoneNumber, value, RaisePropertyChanged());
+        }
+
+        private string _note;
+        public string Note
+        {
+            get => _note;
+            set => this.MutateVerbose(ref _note, value, RaisePropertyChanged());
+        }
+
+        public int UpdateID { get; set; }
+        public DateTime UpdateDateAdded { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private Action<PropertyChangedEventArgs> RaisePropertyChanged() => args => PropertyChanged?.Invoke(this, args);
+    }
+}
