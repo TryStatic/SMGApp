b3bc140e2503ba12e22593badc76127907d21a25
Static <jimakosfb@gmail.com>
2020-05-10 05:47:37 +0300

    Global Exception Logging and Handling

--- SMGApp.WPF/App.xaml.cs
+++ SMGApp.WPF/App.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.ComponentModel.Design;
 using System.Globalization;
+using System.IO;
 using System.Threading;
 using System.Windows;
 using System.Windows.Markup;
+using System.Windows.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using SMGApp.Domain.Models;
 using SMGApp.Domain.Services;
@@ -65,6 +67,18 @@ namespace SMGApp.WPF
             services.AddScoped<MainWindow>(provider => new MainWindow(provider.GetRequiredService<MainViewModel>()));
 
             return services.BuildServiceProvider();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter("error.log", true))
+            {
+                writer.WriteLine($"--------------[{DateTime.Now}]--------------");
+                writer.WriteLine(e.Exception);
+                writer.Write("\n\n");
+            }
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
         }
     }
 }
