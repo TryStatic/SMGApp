ac914f3f497eb93f491ab5bff4a2626171aeaa30
Static <jimakosfb@gmail.com>
2020-05-21 05:57:50 +0300

    Passed Customer and Guarantees Data Services to Guarantee ViewModel

--- SMGApp.WPF/ViewModels/Factories/GuaranteeViewModelFactory.cs
+++ SMGApp.WPF/ViewModels/Factories/GuaranteeViewModelFactory.cs
@@ -1,10 +1,21 @@
-﻿namespace SMGApp.WPF.ViewModels.Factories
+﻿using SMGApp.EntityFramework.Services;
+
+namespace SMGApp.WPF.ViewModels.Factories
 {
     public class GuaranteeViewModelFactory : ISMGAppViewModelFactory<GuaranteeViewModel>
     {
+        private readonly CustomersDataService _customerService;
+        private readonly GuaranteeDataService _guaranteeDataService;
+
+        public GuaranteeViewModelFactory(CustomersDataService customerService, GuaranteeDataService guaranteeDataService)
+        {
+            _customerService = customerService;
+            _guaranteeDataService = guaranteeDataService;
+        }
+
         public GuaranteeViewModel CreateViewModel()
         {
-            return new GuaranteeViewModel();
+            return new GuaranteeViewModel(_customerService, _guaranteeDataService);
         }
     }
 }
--- SMGApp.WPF/ViewModels/GuaranteeViewModel.cs
+++ SMGApp.WPF/ViewModels/GuaranteeViewModel.cs
@@ -1,7 +1,16 @@
-﻿namespace SMGApp.WPF.ViewModels
+﻿using SMGApp.EntityFramework.Services;
+
+namespace SMGApp.WPF.ViewModels
 {
     public class GuaranteeViewModel : ViewModelBase
     {
+        private readonly CustomersDataService _customerService;
+        private readonly GuaranteeDataService _guaranteeDataService;
 
+        public GuaranteeViewModel(CustomersDataService customerService, GuaranteeDataService guaranteeDataService)
+        {
+            _customerService = customerService;
+            _guaranteeDataService = guaranteeDataService;
+        }
     }
 }
