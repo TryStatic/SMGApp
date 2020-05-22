caf902964f84156e528f21d715959933d931a3d4
Static <jimakosfb@gmail.com>
2020-05-22 08:16:20 +0300

    Init edit guarantee edit create dialog

---
+++ SMGApp.WPF/Dialogs/GuaranteeDialogs/GuaranteeDialogView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SMGApp.WPF.Dialogs.GuaranteeDialogs
+{
+    /// <summary>
+    /// Interaction logic for SampleDialog.xaml
+    /// </summary>
+    public partial class GuaranteeDialogView : UserControl
+    {
+        public GuaranteeDialogView()
+        {
+            InitializeComponent();
+        }
+
+        private void Cb_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Cb.IsDropDownOpen = true;
+        }
+    }
+}
---
+++ SMGApp.WPF/Dialogs/GuaranteeDialogs/GuaranteeDialogViewModel.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using SMGApp.Domain.Models;
+using SMGApp.WPF.ViewModels.Util;
+
+namespace SMGApp.WPF.Dialogs.GuaranteeDialogs
+{
+    public class GuaranteeDialogViewModel : INotifyPropertyChanged
+    {
+        // Operation Name
+        public string OperationName { get; set; }
+        public int UpdateID { get; set; }
+        public Customer CustomerBeforeEdit { get; set; }
+        public DateTime UpdateDateAdded { get; set; }
+
+        #region ComboBoxCustomers
+        private ObservableCollection<string> _comboBoxBoundCustomers = new ObservableCollection<string>();
+        public IEnumerable<string> AllCustomers { get; set; }
+        public ObservableCollection<string> ComboBoxBoundCustomers
+        {
+            get => _comboBoxBoundCustomers;
+            set => this.MutateVerbose(ref _comboBoxBoundCustomers, value, RaisePropertyChanged());
+        }
+
+        public GuaranteeDialogViewModel(List<string> customerNames)
+        {
+            AllCustomers = customerNames;
+            ComboBoxBoundCustomers = new ObservableCollection<string>(customerNames);
+        }
+
+        private string _customerName;
+        public string CustomerName
+        {
+            get => _customerName;
+            set
+            {
+                if (_customerName == value) return;
+
+                if (value == null || string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    ComboBoxBoundCustomers = new ObservableCollection<string>(AllCustomers);
+                    return;
+                }
+
+                _customerName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CustomerName)));
+                IEnumerable<string> filteredCustomers = AllCustomers.Where(it => it.ToUpperΝοintonation().Contains(CustomerName.ToUpperΝοintonation()));
+                ComboBoxBoundCustomers = new ObservableCollection<string>(filteredCustomers);
+            }
+        }
+        #endregion
+
+        private string _device;
+        private string _damageDescription;
+        private string _notes;
+        private string _devicePassword;
+        private string _simCode;
+        private string _accountUsername;
+        private string _accountPassword;
+        private bool _chargerIncluded;
+        private bool _bagIncluded;
+        private bool _caseIncluded;
+        private ServiceState _serviceState;
+        private double _cost;
+
+        public string Device
+        {
+            get => _device;
+            set => this.MutateVerbose(ref _device, value, RaisePropertyChanged());
+        }
+
+        public string DamageDescription
+        {
+            get => _damageDescription;
+            set => this.MutateVerbose(ref _damageDescription, value, RaisePropertyChanged());
+
+        }
+
+        public string Notes
+        {
+            get => _notes;
+            set => this.MutateVerbose(ref _notes, value, RaisePropertyChanged());
+        }
+
+        public string DevicePassword
+        {
+            get => _devicePassword;
+            set => this.MutateVerbose(ref _devicePassword, value, RaisePropertyChanged());
+
+        }
+
+        public string SimCode
+        {
+            get => _simCode;
+            set => this.MutateVerbose(ref _simCode, value, RaisePropertyChanged());
+        }
+
+        public string AccountUsername
+        {
+            get => _accountUsername;
+            set => this.MutateVerbose(ref _accountUsername, value, RaisePropertyChanged());
+
+        }
+
+        public string AccountPassword
+        {
+            get => _accountPassword;
+            set => this.MutateVerbose(ref _accountPassword, value, RaisePropertyChanged());
+        }
+
+        public bool ChargerIncluded
+        {
+            get => _chargerIncluded;
+            set => this.MutateVerbose(ref _chargerIncluded, value, RaisePropertyChanged());
+
+        }
+
+        public bool BagIncluded
+        {
+            get => _bagIncluded;
+            set => this.MutateVerbose(ref _bagIncluded, value, RaisePropertyChanged());
+
+        }
+
+        public bool CaseIncluded
+        {
+            get => _caseIncluded;
+            set => this.MutateVerbose(ref _caseIncluded, value, RaisePropertyChanged());
+
+        }
+
+        public ServiceState ServiceState
+        {
+            get => _serviceState;
+            set => this.MutateVerbose(ref _serviceState, value, RaisePropertyChanged());
+        }
+
+        public double Cost
+        {
+            get => _cost;
+            set => this.MutateVerbose(ref _cost, value, RaisePropertyChanged());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private Action<PropertyChangedEventArgs> RaisePropertyChanged() => args => PropertyChanged?.Invoke(this, args);
+    }
+}
