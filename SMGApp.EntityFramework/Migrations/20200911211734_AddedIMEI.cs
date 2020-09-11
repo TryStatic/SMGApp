0079f51d56eb31ea77f8b2ae0c6f96c90e7d5241
Static <jimakosfb@gmail.com>
2020-09-12 00:21:09 +0300

    IMEI migration

---
+++ SMGApp.EntityFramework/Migrations/20200911211734_AddedIMEI.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SMGApp.EntityFramework.Migrations
+{
+    public partial class AddedIMEI : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProductIMEI",
+                table: "Guarantees",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductIMEI",
+                table: "Guarantees");
+        }
+    }
+}
